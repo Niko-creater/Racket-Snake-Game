8286fc8bf8f376943c89b974684f8f04cec5bc58
Peiyu Liu <liupe@usi.ch>
2022-12-12 22:19:36 +0100

    finished

--- snake_inital.rkt
+++ snake_inital.rkt
@@ -26,8 +26,8 @@
 ;    pos: Posn
 ;    col: Color
 ;    ty: Number
-; Interpretation: a food state contains the position of the food, color of the food and a number stand for the type of food.(0 stand for normal food
-;    will increase the length of snake by 1, -1 stand for poison food will decrease the length of snake by 1, 1 stand for double food will increase
+; Interpretation: a food state contains the position of the food, color of the food and a number stand for the type of food.(1 2 stand for normal food
+;    will increase the length of snake by 1, 3 stand for poison food will decrease the length of snake by 1, 4 stand for double food will increase
 ;    the length of snake by 2)
 (struct f_state (pos col ty) #:transparent)
 
@@ -51,7 +51,7 @@
 (define pos_list1 (list (make-posn 720 100) (make-posn 740 100) (make-posn 760 100) (make-posn 780 100) (make-posn 800 100)))
 (define sk_1 (s_state pos_list "right" gap (make-color 0 0 0)))
 (define sk_2 (s_state pos_list1 "right" gap (make-color 0 0 0)))
-(define fd_1 (f_state (make-posn 200 200) (make-color 0 0 0) 0))
+(define fd_1 (f_state (make-posn 200 200) (make-color 0 0 0) 4))
 (define fd_2 (f_state (make-posn 300 300) (make-color 255 0 0) 1))
 (define i (w_state sk_1 (list fd_1)))
 (define i1 (w_state sk_2 (list fd_1 fd_2)))
@@ -93,21 +93,21 @@
 (check-expect (delete_by_value (list fd_1 fd_2) (make-posn 200 200)) (list fd_2))
 (check-expect (delete_by_value (list fd_1 fd_2) (make-posn 300 300)) (list fd_1))
 
-; get_head: List<S_state> -> S_state
+; get_head_s: List<S_state> -> S_state
 ; return the position of last element in s_state list which is defined as the position pf head of the snake.
-; header: (define (get_head sk_1))
+; header: (define (get_head_s sk_1))
 
-(define (get_head s)
+(define (get_head_s s)
   (list-ref (s_state-pos s) (- (length (s_state-pos s)) 1)))
 
-(check-expect (get_head sk_1) (make-posn 180 100))
+(check-expect (get_head_s sk_1) (make-posn 180 100))
 
 ; get_random_food: -> F_state
 ; return a random f_state
 ; header: (define (get_random_food))
 
 (define (get_random_food)
-  (f_state (make-posn (* 20 (random -20 20)) (* 20 (random -20 20))) (make-color (random 0 255) (random 0 255) (random 0 255)) (random -1 1)))
+  (f_state (make-posn (* 20 (random -20 20)) (* 20 (random -20 20))) (make-color (random 0 255) (random 0 255) (random 0 255)) (random 1 5)))
 
 ; get_by_value: List<F_state> Posn -> F_state
 ; return a f_state based on the input position
@@ -136,7 +136,7 @@
                                   [(= (get_index p x) (- (length p) 1)) (make-posn (- (- (/ canvas_w 2) (/ gap 2))) (posn-y x))]
                                   [else (list-ref p (+ 1 (get_index p x)))])) p)))
     (cond
-     [(equal? (- (/ canvas_w 2) (/ gap 2)) (posn-x (get_head s))) (s_state (cr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))]
+     [(equal? (- (/ canvas_w 2) (/ gap 2)) (posn-x (get_head_s s))) (s_state (cr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))]
      [else (s_state (mr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))])))
 
 (check-expect (s_state-pos (move_right sk_1)) (list (make-posn 120 100) (make-posn 140 100) (make-posn 160 100) (make-posn 180 100) (make-posn 200 100)))
@@ -153,7 +153,7 @@
                                   [(= (get_index p x) (- (length p) 1)) (make-posn (- (/ canvas_w 2) (/ gap 2)) (posn-y x))]
                                   [else (list-ref p (+ 1 (get_index p x)))])) p)))
     (cond
-     [(equal? (- (- (/ canvas_w 2) (/ gap 2))) (posn-x (get_head s))) (s_state (cr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))]
+     [(equal? (- (- (/ canvas_w 2) (/ gap 2))) (posn-x (get_head_s s))) (s_state (cr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))]
      [else (s_state (mr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))])))
 
 (check-expect (s_state-pos (move_left sk_1)) (list (make-posn 120 100) (make-posn 140 100) (make-posn 160 100) (make-posn 180 100) (make-posn 160 100)))
@@ -169,7 +169,7 @@
                                   [(= (get_index p x) (- (length p) 1)) (make-posn (posn-x x) (- (/ canvas_h 2) (/ gap 2)))]
                                   [else (list-ref p (+ 1 (get_index p x)))])) p)))
     (cond
-     [(equal? (- (- (/ canvas_h 2) (/ gap 2))) (posn-y (get_head s))) (s_state (cr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))]
+     [(equal? (- (- (/ canvas_h 2) (/ gap 2))) (posn-y (get_head_s s))) (s_state (cr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))]
      [else (s_state (mr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))])))
 
 (check-expect (s_state-pos (move_up sk_1)) (list (make-posn 120 100) (make-posn 140 100) (make-posn 160 100) (make-posn 180 100) (make-posn 180 80)))
@@ -185,7 +185,7 @@
                                   [(= (get_index p x) (- (length p) 1)) (make-posn (posn-x x) (- (- (/ canvas_h 2) (/ gap 2))))]
                                   [else (list-ref p (+ 1 (get_index p x)))])) p)))
     (cond
-     [(equal? (+ (- (/ canvas_h 2) (/ gap 2))) (posn-y (get_head s))) (s_state (cr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))]
+     [(equal? (+ (- (/ canvas_h 2) (/ gap 2))) (posn-y (get_head_s s))) (s_state (cr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))]
      [else (s_state (mr (s_state-pos s)) (s_state-dir s) (s_state-sp s) (s_state-col s))])))
 
 (check-expect (s_state-pos (move_down sk_1)) (list (make-posn 120 100) (make-posn 140 100) (make-posn 160 100) (make-posn 180 100) (make-posn 180 120)))
@@ -216,20 +216,22 @@
   (cond
     [(null? los) cav]
     [(null? (cdr los)) (cond
-                         [(equal? 0 (f_state-ty (car los))) (underlay/offset cav (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_n)]
-                         [(equal? -1 (f_state-ty (car los))) (underlay/offset cav (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_p)]
-                         [(equal? 1 (f_state-ty (car los))) (underlay/offset cav (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_d)])]
+                         [(or (equal? 1 (f_state-ty (car los)))
+                              (equal? 2 (f_state-ty (car los)))) (underlay/offset cav (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_n)]
+                         [(equal? 3 (f_state-ty (car los))) (underlay/offset cav (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_p)]
+                         [(equal? 4 (f_state-ty (car los))) (underlay/offset cav (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_d)])]
     [else (cond
-            [(equal? 0 (f_state-ty (car los))) (underlay/offset (draw_food cav (cdr los)) (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_n)]
-            [(equal? -1 (f_state-ty (car los))) (underlay/offset (draw_food cav (cdr los)) (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_p)]
-            [(equal? 1 (f_state-ty (car los))) (underlay/offset (draw_food cav (cdr los)) (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_d)])]))) 
+            [(or (equal? 1 (f_state-ty (car los)))
+                              (equal? 2 (f_state-ty (car los)))) (underlay/offset (draw_food cav (cdr los)) (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_n)]
+            [(equal? 3 (f_state-ty (car los))) (underlay/offset (draw_food cav (cdr los)) (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_p)]
+            [(equal? 4 (f_state-ty (car los))) (underlay/offset (draw_food cav (cdr los)) (posn-x (f_state-pos (car los))) (posn-y (f_state-pos (car los))) f_d)])]))) 
 
 ; draw_score: Image Number -> Image
 ; paint the score of the game on the canvas
 ; header; (define (draw_score BACKGROUND 0))
 
 (define (draw_score cav score)
-  (underlay/offset cav -430 -370 (text (string-append "Score:  " (number->string (- score 5))) 30 "indigo")))
+  (underlay/offset cav -430 -375 (text (string-append "Score:  " (number->string (- score 5))) 25 "indigo")))
 
 ; render: W_state -> Image
 ; return the image based the current w_state
@@ -301,50 +303,60 @@
 (check-expect (eat? i) #f)
 
 ;; Three eat function
+(define (get_head_w w) (list-ref (s_state-pos (w_state-snake w)) (- (length (s_state-pos (w_state-snake w))) 1)))
+
+(define (right_to_head w n)
+  (cond
+    [(= 1 n) (append (s_state-pos (w_state-snake w)) (list (make-posn (+ gap (posn-x (get_head_w w))) (posn-y (get_head_w w)))))]
+    [else (append (right_to_head w (- n 1)) (list (make-posn (+ (* n gap) (posn-x (get_head_w w))) (posn-y (get_head_w w)))))]))
+
+(define (left_to_head w n)
+  (cond
+    [(= 1 n) (append (s_state-pos (w_state-snake w)) (list (make-posn (+ (- gap) (posn-x (get_head_w w))) (posn-y (get_head_w w)))))]
+    [else (append (left_to_head w (- n 1)) (list (make-posn (+ (- (* n gap)) (posn-x (get_head_w w))) (posn-y (get_head_w w)))))]))
+
+(define (up_to_head w n)
+  (cond
+    [(= 1 n) (append (s_state-pos (w_state-snake w)) (list (make-posn (posn-x (get_head_w w)) (+ (- gap)(posn-y (get_head_w w))))))]
+    [else (append (up_to_head w (- n 1)) (list (make-posn (posn-x (get_head_w w)) (+ (- (* n gap))(posn-y (get_head_w w))))))])) 
+
+(define (down_to_head w n)
+  (cond
+    [(= 1 n) (append (s_state-pos (w_state-snake w)) (list (make-posn (posn-x (get_head_w w)) (+ gap (posn-y (get_head_w w))))))]
+    [else (append (down_to_head w (- n 1)) (list (make-posn (posn-x (get_head_w w)) (+ (* n gap) (posn-y (get_head_w w))))))])) 
+
+(define (update_food w n)
+  (cond
+   [(= 1 n) (append (list (get_random_food)) (delete_by_value (w_state-food w) (get_head_w w)))]
+   [else (append (list (get_random_food)) (update_food w (- n 1)))]))
+
+(define (pop_head w) (remove (get_head_w w) (s_state-pos (w_state-snake w))))
 
 ; eat-normal: W_state -> W_state
 ; append a new part on the snake body
 ; header: (define (eat-normal i))
 (define (eat-normal w)
-  (local ((define last_element (list-ref (s_state-pos (w_state-snake w)) (- (length (s_state-pos (w_state-snake w))) 1)))
-          (define right_to_head (append (s_state-pos (w_state-snake w)) (list (make-posn (+ gap (posn-x last_element)) (posn-y last_element)))))
-          (define left_to_head (append (s_state-pos (w_state-snake w)) (list (make-posn (+ (- gap) (posn-x last_element)) (posn-y last_element)))))
-          (define up_to_head (append (s_state-pos (w_state-snake w)) (list (make-posn (posn-x last_element) (+ (- gap)(posn-y last_element))))))
-          (define down_to_head (append (s_state-pos (w_state-snake w)) (list (make-posn (posn-x last_element) (+ gap (posn-y last_element))))))
-          (define (update_food f_list) (append (list (get_random_food)) (delete_by_value (w_state-food w) last_element)))
-          )
   (cond
-    [(equal? "right" (s_state-dir (w_state-snake w))) (w_state (s_state right_to_head "right" gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))]
-    [(equal? "left" (s_state-dir (w_state-snake w))) (w_state (s_state left_to_head "left" gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))]
-    [(equal? "up" (s_state-dir (w_state-snake w))) (w_state (s_state up_to_head "up" gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))]
-    [(equal? "down" (s_state-dir (w_state-snake w))) (w_state (s_state down_to_head "down" gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))])))
+    [(equal? "right" (s_state-dir (w_state-snake w))) (w_state (s_state (right_to_head w 1) "right" gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 1))]
+    [(equal? "left" (s_state-dir (w_state-snake w))) (w_state (s_state (left_to_head w 1) "left" gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 1))]
+    [(equal? "up" (s_state-dir (w_state-snake w))) (w_state (s_state (up_to_head w 1) "up" gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 1))]
+    [(equal? "down" (s_state-dir (w_state-snake w))) (w_state (s_state (down_to_head w 1) "down" gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 1))]))
 
 ; eat-poison: W_state -> W_state
 ; pop the head part of the snake body
 ; header: (define (eat-poison i))
 (define (eat-poison w)
-  (local ((define last_element (list-ref (s_state-pos (w_state-snake w)) (- (length (s_state-pos (w_state-snake w))) 1)))
-          (define pop_head (remove last_element (s_state-pos (w_state-snake w))))
-          (define (update_food f_list) (append (list (get_random_food) (get_random_food)) (delete_by_value (w_state-food w) last_element)))
-          )
-  (w_state (s_state pop_head (s_state-dir (w_state-snake w)) gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))))
+  (w_state (s_state (pop_head w) (s_state-dir (w_state-snake w)) gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 2)))
 
 ; eat-double: W_state -> W_state
 ; append two new parts on the snake body
 ; header: (define (eat-double i))
 (define (eat-double w)
-  (local ((define last_element (list-ref (s_state-pos (w_state-snake w)) (- (length (s_state-pos (w_state-snake w))) 1)))
-          (define right_to_head (append (s_state-pos (w_state-snake w)) (list (make-posn (+ gap (posn-x last_element)) (posn-y last_element)) (make-posn (+ (* 2 gap) (posn-x last_element)) (posn-y last_element)))))
-          (define left_to_head (append (s_state-pos (w_state-snake w)) (list (make-posn (+ (- gap) (posn-x last_element)) (posn-y last_element)) (make-posn (+ (- (* 2 gap)) (posn-x last_element)) (posn-y last_element)))))
-          (define up_to_head (append (s_state-pos (w_state-snake w)) (list (make-posn (posn-x last_element) (+ (- gap)(posn-y last_element))) (make-posn (posn-x last_element) (+ (- (* 2 gap)) (posn-y last_element))))))
-          (define down_to_head (append (s_state-pos (w_state-snake w)) (list (make-posn (posn-x last_element) (+ gap (posn-y last_element))) (make-posn (posn-x last_element) (+ (* 2 gap) (posn-y last_element))))))
-          (define (update_food f_list) (append (list (get_random_food)) (delete_by_value (w_state-food w) last_element)))
-          )
   (cond
-    [(equal? "right" (s_state-dir (w_state-snake w))) (w_state (s_state right_to_head "right" gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))]
-    [(equal? "left" (s_state-dir (w_state-snake w))) (w_state (s_state left_to_head "left" gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))]
-    [(equal? "up" (s_state-dir (w_state-snake w))) (w_state (s_state up_to_head "up" gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))]
-    [(equal? "down" (s_state-dir (w_state-snake w))) (w_state (s_state down_to_head "down" gap (f_state-col (get_by_value (w_state-food w) last_element))) (update_food (w_state-food w)))])))
+    [(equal? "right" (s_state-dir (w_state-snake w))) (w_state (s_state (right_to_head w 2) "right" gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 1))]
+    [(equal? "left" (s_state-dir (w_state-snake w))) (w_state (s_state (left_to_head w 2) "left" gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 1))]
+    [(equal? "up" (s_state-dir (w_state-snake w))) (w_state (s_state (up_to_head w 2) "up" gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 1))]
+    [(equal? "down" (s_state-dir (w_state-snake w))) (w_state (s_state (down_to_head w 2) "down" gap (f_state-col (get_by_value (w_state-food w) (get_head_w w)))) (update_food w 1))]))
 (test)
 
 ;; Tick
@@ -354,16 +366,16 @@
 ; header: (define (tick w))
 
 (define (tick w)
-  (local ((define last_element (list-ref (s_state-pos (w_state-snake w)) (- (length (s_state-pos (w_state-snake w))) 1))))
           (cond
             [(eat? w) (cond
-                        [(equal? -1 (f_state-ty (get_by_value (w_state-food w) last_element))) (eat-poison w)]
-                        [(equal? 0 (f_state-ty (get_by_value (w_state-food w) last_element))) (eat-normal w)]
-                        [(equal? 1 (f_state-ty (get_by_value (w_state-food w) last_element))) (eat-double w)])]
+                        [(equal? 3 (f_state-ty (get_by_value (w_state-food w) (get_head_w w)))) (eat-poison w)]
+                        [(or (equal? 1 (f_state-ty (get_by_value (w_state-food w) (get_head_w w))))
+                             (equal? 2 (f_state-ty (get_by_value (w_state-food w) (get_head_w w))))) (eat-normal w)]
+                        [(equal? 4 (f_state-ty (get_by_value (w_state-food w) (get_head_w w)))) (eat-double w)])]
             [(equal? "right" (s_state-dir (w_state-snake w))) (w_state (move_right (w_state-snake w)) (w_state-food w))]
             [(equal?  "left" (s_state-dir (w_state-snake w))) (w_state (move_left (w_state-snake w)) (w_state-food w))]
             [(equal?  "up" (s_state-dir (w_state-snake w))) (w_state (move_up (w_state-snake w)) (w_state-food w))]
-            [(equal?  "down" (s_state-dir (w_state-snake w))) (w_state (move_down (w_state-snake w)) (w_state-food w))])))
+            [(equal?  "down" (s_state-dir (w_state-snake w))) (w_state (move_down (w_state-snake w)) (w_state-food w))]))
 
 (define (exist? los h)
   (cond
@@ -376,8 +388,8 @@
 ; header: (define (quit? w))
   
 (define (quit? w)
-  (local ((define last_element (list-ref (s_state-pos (w_state-snake w)) (- (length (s_state-pos (w_state-snake w))) 1)))
-          (define (colli? s) (exist? (s_state-pos s) last_element))
+  (local (
+          (define (colli? s) (exist? (s_state-pos s) (get_head_w w)))
           )
     (cond
       [(colli? (w_state-snake w)) #t]
